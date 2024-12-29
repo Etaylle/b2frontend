62205eebb4153bd4655fa04289933f14c0db1d75
urosm <um.uros20@gmail.com>
2024-12-29 16:30:42 +0100

    Initial commit for frontend

--- scripts/index.js
+++ scripts/index.js
@@ -335,7 +335,7 @@ const authManager = {
       if (response.ok) {
         showNotification('Login successful!', 'success');
         uiManager.closeLogin();
-        window.location.reload();
+        //window.location.reload();
       } else {
         showNotification(data.message, 'error');
       }
