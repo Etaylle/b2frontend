561e6d6d04c414075300c5e6e1a2f93adb74a878
urosm <um.uros20@gmail.com>
2025-01-11 09:41:56 +0100

    first commit

--- scripts/productGridInit.js
+++ scripts/productGridInit.js
@@ -1,69 +1,28 @@
-// productGridInit.js
 const initializeProductGrid = (products) => {
-    // Create a wrapper for the React component
-    class ProductGridWrapper extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                products: props.products,
-                cryptoRates: {
-                    BTC: 0,
-                    ETH: 0
-                },
-                selectedCurrency: 'USD'
-            };
-        }
-
-        componentDidMount() {
-            this.fetchCryptoRates();
-            // Update rates every minute
-            this.interval = setInterval(this.fetchCryptoRates, 60000);
-        }
-
-        componentWillUnmount() {
-            if (this.interval) {
-                clearInterval(this.interval);
-            }
-        }
-
-        fetchCryptoRates = async () => {
-            try {
-                const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
-                const data = await response.json();
-                this.setState({
-                    cryptoRates: {
-                        BTC: data.bitcoin.usd,
-                        ETH: data.ethereum.usd
-                    }
-                });
-            } catch (error) {
-                console.error('Error fetching crypto rates:', error);
+    const rootElement = document.getElementById('product-grid-root');
+    
+    // Create wrapper component to handle cart integration
+    const ProductGridWrapper = () => {
+        const handleAddToCart = (product) => {
+            // Integration with your existing cart system
+            if (typeof cartManager !== 'undefined' && cartManager.addItem) {
+                cartManager.addItem(product.product_id);
             }
         };
 
-        render() {
-            return React.createElement(ProductGrid, {
-                products: this.state.products,
-                cryptoRates: this.state.cryptoRates,
-                selectedCurrency: this.state.selectedCurrency,
-                onCurrencyChange: (currency) => this.setState({ selectedCurrency: currency })
-            });
-        }
-    }
+        return React.createElement(ProductGrid, {
+            products: products,
+            onAddToCart: handleAddToCart
+        });
+    };
 
-    // Mount the React component
-    const rootElement = document.getElementById('product-grid-root');
     ReactDOM.render(
-        React.createElement(ProductGridWrapper, { products: products }),
+        React.createElement(ProductGridWrapper),
         rootElement
     );
 };
 
 // Function to update products
-const updateProductGrid = (newProducts) => {
-    const rootElement = document.getElementById('product-grid-root');
-    ReactDOM.render(
-        React.createElement(ProductGridWrapper, { products: newProducts }),
-        rootElement
-    );
+const updateProducts = (newProducts) => {
+    initializeProductGrid(newProducts);
 };
