c23ff3f13eba70db89c7a5337f32bd2b996cecdf
urosm <um.uros20@gmail.com>
2025-01-21 07:58:43 +0100

    quantity fix

--- scripts/index.js
+++ scripts/index.js
@@ -2733,7 +2733,7 @@ async searchProducts(query) {
 
       // Show no results message in current language
       if (filteredProducts.length === 0) {
-        const noResultsMessage = i18nManager.translate('ui.messages.noProductsFound');
+        const noResultsMessage = i18nManager.translate('noProductsFound');
         showNotification(noResultsMessage, 'info');
       }
 
